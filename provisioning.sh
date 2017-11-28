27a8a132bc1b2047768539e7c08d77840924e01b
fasiahmed <root@localhost.localdomain>
2017-11-28 16:24:33 +0100

    change to provisioning

--- provisioning.sh
+++ provisioning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#### ====> Install required packages
+#### ====> Install required packages <=======================================
 sudo apt update
 sleep 60
 sudo apt-get -y install software-properties-common python-software-properties
@@ -18,7 +18,7 @@ sudo apt-get install -y nginx
 sleep 10
 
 
-#### ====> Create a path and install jetty-distribution-9
+#### ====> Create a path and install jetty-distribution-9 <============================================
 sudo apt-get install -y unzip
 sleep 10
 sudo wget http://central.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.3.22.v20171030/jetty-distribution-9.3.22.v20171030.zip -P /home/ubuntu/
@@ -29,13 +29,13 @@ sudo mv jetty-distribution-9.3.22.v20171030 /var/www/jetty
 sleep 20
 cd /var/www/jetty/demo-base/
 
-#### ====> Copy the builds to respective folders of tw-docker-infra for use in docker containers
+#### ====> copy the default file which pass the proxy 8080 to port 80 <===================================
 sudo rm -f /etc/nginx/sites-available/default
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo cp /home/ubuntu/demo/default /etc/nginx/sites-available/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
-#### ====> Set cloud instance's PUBLIC_IP as environment variable to be used in static URL to render css
+#### ====> Set cloud instance's PUBLIC_IP as environment variable to be used in static URL to render css  <============================================
 #export PUBLIC_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4) >> /home/ubuntu/.bash_profile
 #sleep 10                          # Wait for environment variable to be properly set
 
